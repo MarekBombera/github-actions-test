53ed303d6aeabf5d4f98e2dee72ff5d5cd483d63
Marek Bombera <bomber.marek@gmail.com>
2022-05-05 19:27:37 +0200

    added thank you to pull requests

--- src/action.js
+++ src/action.js
@@ -1,5 +1,18 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 const run = async () => {
-	console.log('Hello World');
+	const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+	const octokit = github.getOctokit(GITHUB_TOKEN);
+
+	const { context = {} } = github;
+	const { pull_request } = context.payload;
+
+	await octokit.issues.createComment({
+		...context.repo,
+		issue_number: pull_request.number,
+		body: 'Thank you for submitting a pull request',
+	});
 };
 
 run();
