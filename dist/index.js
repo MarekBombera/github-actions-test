71e42747836df9357e5d276aab31161eca8099b4
Marek Bombera <bomber.marek@gmail.com>
2022-05-05 19:17:57 +0200

    adding onOpened action

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+const run = async () => {
+	console.log('Hello World');
+};
+
+run();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
